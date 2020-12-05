39146d5240f77d4372ca7fa945f3f9d4e354f2c8
Martinguinazu530 <75535262+Martinguinazu530@users.noreply.github.com>
2020-12-05 16:24:02 -0300

    Create style.css

---
+++ style.css
@@ -0,0 +1,99 @@
+body {
+	background-image: url('imagenes/13.0.jpg');
+	background-size: cover;
+	color: white;
+	margin: 0;
+	background-position: center;
+}
+
+h1, h2 {
+	font-family: 'Abril'
+}
+
+.container {
+	font-size: 19px;
+	margin: 16px;
+	color: white;
+}
+
+.banner {
+	background-size: cover;
+	background-position: center;
+	padding: 140px 0;
+	text-align: center;
+}
+
+.banner h1 {
+	font-size: 80px;
+	margin: 4px;
+}
+
+.banner h2 {
+	font-size: 26px;
+	color: #c0c6ed;
+}
+
+.banner marquee {
+	color: white;
+	height: 55px;
+	font-size: 50px;
+	text-align: center;
+}
+
+.banner img {
+	height: 50px;
+	width: 60px;
+	border-radius: 17px;
+	background-position: center;
+	text-align: center;
+}
+
+
+.btn-blue {
+	padding: 10px 10px;
+	border-radius: 100px;
+	display: inline-block;
+	text-decoration: none;
+	transition: 0.2s;
+	font-size: 7px;
+	font-weight: bold;
+	border: none;
+}
+.primera  {
+	border-radius: 40px;
+	margin: 60px auto;
+	background-position: center;
+}
+
+.primera img {
+	width: 500px;
+	height: 500px;
+	border-radius: 20px;
+	margin: 90px auto;
+}
+
+.segunda {
+	border-radius: 40px;
+	margin: 60px auto;
+	background-position: center;
+}
+
+.segunda img {
+	width: 500px;
+	height: 500px;
+	border-radius: 20px;
+	margin: 90px auto;
+}
+
+.footer {
+	background: black;
+	color: white;
+	padding: 40px 0;
+	font-size: 20px;
+}
+
+.los {
+	display: grid;
+	grid-template-columns: 1fr 1fr;
+	grid-gap: 0;
+}
